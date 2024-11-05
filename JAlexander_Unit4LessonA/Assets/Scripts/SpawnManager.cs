0b96229c3e777e5cc9d689f7451f1b9b5a3e8a6f
Joshalex1 <161536154+Joshalex1@users.noreply.github.com>
2024-11-04 22:57:36 -0800

    unit4lessonD

--- JAlexander_Unit4LessonA/Assets/Scripts/SpawnManager.cs
+++ JAlexander_Unit4LessonA/Assets/Scripts/SpawnManager.cs
@@ -6,12 +6,40 @@ public class SpawnManager : MonoBehaviour
 {
     public GameObject enemyPrefab;
     private float spawnRange = 8.5f;
+
+    public GameObject poweUpPrefab;
+    private int enemyCount;
+
+    private int waveNumber = 1;
     // Start is called before the first frame update
     void Start()
     {
+        SpawnWave(waveNumber);
        
-        Instantiate(enemyPrefab, GenerateSpawnPosition(), enemyPrefab.transform.rotation);
     }
+
+    private void Update()
+    {
+        enemyCount = FindObjectsOfType<Enemy>().Length;
+        if(enemyCount == 0)
+        {
+            waveNumber++;
+            SpawnWave(waveNumber);
+        }
+    }
+
+    void SpawnWave(int enemyNum)
+    {
+        Instantiate(poweUpPrefab, GenerateSpawnPosition(), poweUpPrefab.transform.rotation);
+
+        for (int i = 0; i < enemyNum; i++)
+        {
+            Instantiate(enemyPrefab, GenerateSpawnPosition(), enemyPrefab.transform.rotation);
+        }
+
+    }
+
+
 
     Vector3 GenerateSpawnPosition()
     {
@@ -22,9 +50,5 @@ public class SpawnManager : MonoBehaviour
         Vector3 spawnPos = new Vector3(xPos, enemyPrefab.transform.position.y, zPos);
         return spawnPos;
     }
-    // Update is called once per frame
-    void Update()
-    {
-       
-    }
+    
 }
